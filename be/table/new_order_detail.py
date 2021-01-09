0907b44441b753c27ae22a75f78e595c5fea281a
CharlesDDDD <10185101155@stu.ecnu.edu.cn>
2021-01-09 15:18:12 +0800

    订单table加了一列 state

--- be/table/new_order_detail.py
+++ be/table/new_order_detail.py
@@ -8,12 +8,15 @@ class New_Order_Detail(Base):
     book_id = Column(String(240), primary_key=True)
     count = Column(Integer, nullable=False)
     price = Column(Integer, nullable=False)
+    # state 0:未付款 1:已付款 2：已发货 3：已收货
+    state = Column(Integer, nullable=False)
 
-    def __init__(self, order_id=None, book_id=None, count=None, price=None):
+    def __init__(self, order_id=None, book_id=None, count=None, price=None, state=0):
         self.order_id = order_id
         self.book_id = book_id
         self.count = count
         self.price = price
+        self.state = state
 
     def __repr__(self):
-        return '<User %r,%r,%r,%r>' % (self.order_id, self.book_id, self.count, self.price)
+        return '<User %r,%r,%r,%r,%r>' % (self.order_id, self.book_id, self.count, self.price, self.state)
