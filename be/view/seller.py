fa285986bc4c08cfef8ff5b7528c0b2e74bdd7ec
CharlesDDDD <10185101155@stu.ecnu.edu.cn>
2021-01-09 15:31:09 +0800

    增加了卖家http接口

--- be/view/seller.py
+++ be/view/seller.py
@@ -40,3 +40,14 @@ def add_stock_level():
     code, message = s.add_stock_level(user_id, store_id, book_id, add_num)
 
     return jsonify({"message": message}), code
+
+
+@bp_seller.route("/send_stock", methods=["POST"])
+def send_stock():
+    user_id: str = request.json.get("user_id")
+    order_id: str = request.json.get("order_id")
+
+    s = seller.Seller()
+    code, message = s.send_stock(user_id=user_id, order_id=order_id)
+
+    return jsonify({"message": message}), code
