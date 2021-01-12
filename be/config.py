5d80d7b6b6bc316e70d129744e47dac0d1f41bdf
CharlesDDDD <10185101155@stu.ecnu.edu.cn>
2021-01-12 09:14:56 +0800

    修改配置方式 使用nacos进行配置

--- be/config.py
+++ be/config.py
@@ -1,16 +1,20 @@
-DIALECT = 'mysql'
-DRIVER = 'pymysql'
-USERNAME = 'root'
-PASSWORD = 'root'
-HOST = '127.0.0.1'
-PORT = '3306'
-DATABASE = 'test'
+# DIALECT = 'mysql'
+# DRIVER = 'pymysql'
+# USERNAME = 'root'
+# PASSWORD = 'root'
+# HOST = '127.0.0.1'
+# PORT = '3306'
+# DATABASE = 'test'
+#
+# SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}?charset=utf8'.format(
+#     DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE
+# )
+# SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+# SQLALCHEMY_TRACK_MODIFICATIONS = True
+#
+# SQLALCHEMY_POOL_SIZE = 10
+# SQLALCHEMY_MAX_OVERFLOW = 5
 
-SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}?charset=utf8'.format(
-    DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE
-)
-SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-SQLALCHEMY_POOL_SIZE = 10
-SQLALCHEMY_MAX_OVERFLOW = 5
+# logger.info('建立Nacos连接，address：&#123;&#125;，namespace：&#123;&#125;'.format(SERVER_ADDRESSES, NAMESPACE))
+# self.client = nacos.NacosClient(SERVER_ADDRESSES, namespace=NAMESPACE)
+# self.connect_bool = True
